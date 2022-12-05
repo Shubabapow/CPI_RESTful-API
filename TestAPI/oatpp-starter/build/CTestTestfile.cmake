abb525625ee94dc88cd8ded38bca4ff74fba798f
Joey Garver <34845562+Shubabapow@users.noreply.github.com>
2022-12-05 09:59:52 -0600

    Delete TestAPI directory

--- TestAPI/oatpp-starter/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspaces/RestAPI/TestAPI/oatpp-starter
-# Build directory: C:/workspaces/RestAPI/TestAPI/oatpp-starter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(project-tests "my-project-test")
-set_tests_properties(project-tests PROPERTIES  _BACKTRACE_TRIPLES "C:/workspaces/RestAPI/TestAPI/oatpp-starter/CMakeLists.txt;53;add_test;C:/workspaces/RestAPI/TestAPI/oatpp-starter/CMakeLists.txt;0;")
